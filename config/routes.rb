3b28b6745e3ac8b76c3e8e601827fcc41b6f34a2
RamonaS2 <bandeiraramona2@gmail.com>
2023-02-28 18:06:06 -0300

    adicionando logoff

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
   namespace :users_backoffice do
     get 'welcome/index'
   end
-  get 'welcome/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  get 'inicio', to: 'site/welcome#index'
+  get 'backoffice', to: 'users_backoffice/welcome#index'
 
   root to: 'site/welcome#index'
 
